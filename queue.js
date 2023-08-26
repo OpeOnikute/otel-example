115e05c95ef6dbaff01d63ca1aa9a0693ca0eaaa
Opeyemi Onikute <opeyemionikute@yahoo.com>
2023-08-26 12:40:51 +0100

    first commit

---
+++ queue.js
@@ -0,0 +1,26 @@
+const amqp = require('amqplib');
+
+class Queue {
+
+    queue = "task_queue";
+    
+    async send (payload) {
+        const self = this;
+        const queue = self.queue;
+        const connection = await amqp.connect('amqp://localhost');
+        const ch = await connection.createChannel();
+        ch.assertQueue(self.queue, {
+            durable: true
+        });
+        const payloadStr = JSON.stringify(payload);
+        await ch.sendToQueue(queue, Buffer.from(payloadStr), {
+            persistent: true
+        });
+        console.log(" [x] Sent '%s'", payloadStr);
+        // connection.close();
+    }   
+}
+
+
+
+module.exports = new Queue();
